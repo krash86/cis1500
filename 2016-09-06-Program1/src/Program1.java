a06358e84a296bc45d1c0460de525f57c93cde26
David Kataja Jr <krash86.7@gmail.com>
2016-09-06 11:45:27 -0400

    2016-09-06 - Program1 Homework
    Simple println program

---
+++ 2016-09-06-Program1/src/Program1.java
@@ -0,0 +1,21 @@
+/**
+ * First homework assignment for Java class
+ * 
+ * @author David Kataja Jr
+ * @version 2016 Sep 6 CIS 1500
+ */
+public class Program1 {
+	public static void main(String[] args) {
+		System.out.println("Name:");
+		System.out.println("\tDavid Kataja");
+		System.out.println("Major:");
+		System.out.println("\tAAS-Software Engineering");
+		System.out.println("Reason for taking course:");
+		System.out.println("\tDegree requirement and personal gain");
+		System.out.println("Pace of course:");
+		System.out.println("\tSlow for me");
+		System.out.println("General feedback:");
+		System.out.println("\tI am looking forward to learning Java");
+	}
+
+}
