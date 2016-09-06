579e356b6e450427a435aaaddb5efa52182ecc58
David Kataja Jr <krash86.7@gmail.com>
2016-09-06 11:26:08 -0400

    2016-09-06 DayTwo Demo
    Two line print with dialog box
    Comments in-line
    Javadoc comments

---
+++ 2016-09-06-Day2/src/DayTwo.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+/**
+ * A simple second day demo program!
+ * 
+ * @author David Kataja Jr.
+ * @version Sep 6 2016
+ * CIS 1500
+ */
+
+public class DayTwo {
+
+	// variables for data
+
+	// instructions for manipulating data
+
+	public static void main(String[] args) {
+		System.out.println("Alan Jackson");
+		System.out.print("\tI love Java!");
+		JOptionPane.showMessageDialog(null, "My first dialog!");
+	}
+}
